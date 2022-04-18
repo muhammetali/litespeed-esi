4e2411c6c9d732ac7426ccd241fea76c32a0ec59
Muhammet Ali <gmuhammetali@gmail.com>
2022-04-18 06:11:21 +0300

    Create meta.php

---
+++ meta.php
@@ -0,0 +1,6 @@
+<div class="cym-viewcount-wrapper">
+   <div class="cym-viewcount">
+              <?php echo apply_filters( 'litespeed_esi_url', 'cym_esi_block', 'Cym ESI block' ); ?>
+   </div>
+
+</div>
